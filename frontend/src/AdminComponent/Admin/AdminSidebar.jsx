fbc12c37d00d6826abd017b09b9be0adcf24f694
Sayak Banerjee <69157316+gits-up@users.noreply.github.com>
2024-09-16 23:56:40 +0530

    Updated

--- frontend/src/AdminComponent/Admin/AdminSidebar.jsx
+++ frontend/src/AdminComponent/Admin/AdminSidebar.jsx
@@ -27,7 +27,7 @@ export const AdminSidebar = ({ handleClose }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleNavigate = (item) => {
-    navigate(`/admin/restaurans${item.path}`);
+    navigate(`/admin/restaurants${item.path}`);
     if (item.title === "Logout") {
       navigate("/");
       dispatch(logout());
